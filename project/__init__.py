5939d006466affda97f2bf67451a7df3dfeb067f
Bob Belderbos <bobbelderbos@gmail.com>
2020-12-08 19:13:06 +0100

    remove unused code, add article comment, add requirements

--- project/__init__.py
+++ project/__init__.py
@@ -5,6 +5,9 @@ from flask import Flask, jsonify, render_template
 
 from .services import create_customer
 
+# awesome flask stripe guide:
+# https://testdriven.io/blog/flask-stripe-tutorial/
+
 stripe_keys = {
     "secret_key": os.environ["STRIPE_SECRET_KEY"],
     "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
@@ -14,14 +17,8 @@ stripe.api_key = stripe_keys["secret_key"]
 
 # https://flask.palletsprojects.com/en/1.1.x/patterns/appfactories/
 
-def create_app(config_filename=None):
+def create_app():
     app = Flask(__name__)
-    if config_filename:
-        app.config.from_pyfile(config_filename)
-
-    @app.route("/")
-    def index():
-        return render_template("index.html")
 
     @app.route("/call-stripe")
     @app.route("/call-stripe/<name>")
